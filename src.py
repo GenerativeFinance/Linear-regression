d1c6990246fdc06626e33e33482f66f7dd82d1fc
Guillaume Lauzier <55703540+guillaumelauzier@users.noreply.github.com>
2023-02-12 20:43:29 +0100

    Update src.py

--- src.py
+++ src.py
@@ -15,3 +15,14 @@ if __name__ == "__main__":
     data = [(1, 2), (2, 3), (3, 4), (4, 5)]
     m, b = linear_regression(data)
     print("y = {0}x + {1}".format(m, b))
+
+"""
+
+In this implementation, linear_regression takes a list of tuples as 
+input, where each tuple represents a single data point with the x-value 
+and y-value. The function returns a tuple with two elements, representing 
+the slope and the intercept of the regression line. The if __name__ == "__main__" 
+block shows an example usage of the linear_regression function by creating a list 
+of data points and calling the function.
+
+"""
